bfceeb2b7cfb37c7f53937ad31876cec503a245e
Dieter Weber <d.weber@fz-juelich.de>
2024-09-03 10:52:35 +0200

    WIP runtime validation
    Trying to follow https://docs.pydantic.dev/latest/concepts/types/#custom-types
    
    * Validators etc are attached with annotations
    * Managed to serialize
    * Deserialization yields lists and not `pint.Quantity`, needs to be added somehow?
    * Validation only works when the list case is validated, not the one with quantities. It should ideally not even create invalid parameters, or at least catch issues on explicit validation

--- src/libertem_schema/__init__.py
+++ src/libertem_schema/__init__.py
@@ -1,36 +1,91 @@
-from pydantic import BaseModel
+from typing import Any, Tuple
+from numbers import Number
+
+from typing_extensions import Annotated
+from pydantic_core import core_schema
+from pydantic import (
+    BaseModel,
+    GetCoreSchemaHandler,
+    GetJsonSchemaHandler,
+    ValidationError,
+)
+from pydantic.json_schema import JsonSchemaValue
 import pint
 
 
 __version__ = '0.1.0.dev0'
 
-
-# From https://github.com/hgrecco/pint/issues/1166#issuecomment-1116309404
-class PintType:
-    Q = pint.Quantity
-
-    def __init__(self, q_check: str):
-        self.q_check = q_check
-
-    def __get_validators__(self):
-        yield self.validate
-
-    def validate(self, v, validation_info):
-        q = self.Q(v)
-        assert q.check(self.q_check), f"Dimensionality must be {self.q_check}"
-        return q
+ureg = pint.UnitRegistry()
 
 
-Length = PintType("[length]")
-Angle = PintType("")
-Pixel = PintType("[printing_unit]")
+class DimensionError(ValueError):
+    pass
+
+
+def _get_annotation(reference: pint.Quantity):
+    
+    dimensionality = reference.dimensionality
+
+    class Annotation:
+        @classmethod
+        def __get_pydantic_core_schema__(
+            cls,
+            _source_type: Any,
+            _handler: GetCoreSchemaHandler,
+        ) -> core_schema.CoreSchema:
+            def validate_from_tuple(value: Tuple[Number, str]) -> pint.Quantity:
+                m, u = value
+                quantity = m * ureg(u)
+                result = pint.Quantity(quantity)
+                print("debug", quantity, dimensionality)
+                if not result.check(dimensionality):
+                    raise DimensionError(
+                        f"Dimensionality mismatch: Type {type(result)} expected {dimensionality}."
+                    )
+                return result
+
+            from_tuple_schema = core_schema.chain_schema(
+                [
+                    core_schema.tuple_positional_schema(items_schema=[
+                        core_schema.float_schema(),
+                        core_schema.str_schema()
+                    ]),
+                    core_schema.no_info_plain_validator_function(validate_from_tuple),
+                ]
+            )
+
+            return core_schema.json_or_python_schema(
+                json_schema=from_tuple_schema,
+                python_schema=core_schema.union_schema(
+                    [
+                        # check if it's an instance first before doing any further work
+                        core_schema.is_instance_schema(pint.Quantity),
+                        from_tuple_schema,
+                    ]
+                ),
+                serialization=core_schema.plain_serializer_function_ser_schema(
+                    lambda instance: (float(instance.m), str(instance.u))
+                ),
+            )
+    return Annotation
+
+
+Length = Annotated[
+    pint.Quantity, _get_annotation(ureg.meter)
+]
+Angle = Annotated[
+    pint.Quantity, _get_annotation(ureg.radian)
+]
+Pixel = Annotated[
+    pint.Quantity, _get_annotation(ureg.pixel)
+]
 
 
 class Simple4DSTEMParams(BaseModel):
     '''
     Basic calibration parameters of a strongly simplified model
     of a 4D STEM experiment.
-    
+
     See https://github.com/LiberTEM/Microscope-Calibration
     and https://arxiv.org/abs/2403.08538
     for the technical details.
@@ -44,8 +99,3 @@ class Simple4DSTEMParams(BaseModel):
     cx: Pixel
     scan_rotation: Angle
     flip_y: bool
-
-    class Config:
-        json_encoders = {
-            pint.Quantity: str
-        }
--- tests/test_schemas.py
+++ tests/test_schemas.py
@@ -1,7 +1,11 @@
+import pprint
+
+import pytest
+
 from pint import UnitRegistry
-from libertem_schema import Simple4DSTEMParams
 from pydantic_core import from_json
 
+from libertem_schema import Simple4DSTEMParams, DimensionError
 
 ureg = UnitRegistry()
 
@@ -20,4 +24,28 @@ def test_smoke():
         cx=(32 - 2) * ureg.pixel,
     )
     as_json = params.model_dump_json()
-    assert Simple4DSTEMParams.model_validate(from_json(as_json))
+    print(as_json)
+    from_j = from_json(as_json)
+    assert Simple4DSTEMParams.model_validate(from_j)
+
+
+def test_dimensionality():
+    params = Simple4DSTEMParams(
+        overfocus=0.0015 * ureg.degree,  # mismatch
+        scan_pixel_pitch=0.000001 * ureg.meter,
+        camera_length=0.15 * ureg.meter,
+        detector_pixel_pitch=0.000050 * ureg.meter,
+        semiconv=0.020 * ureg.radian,  # rad
+        scan_rotation=330. * ureg.degree,
+        flip_y=False,
+        # Offset to avoid subchip gap in butted detectors
+        cy=(32 - 2) * ureg.pixel,
+        cx=(32 - 2) * ureg.pixel,
+    )
+    pprint.pprint(params)
+    assert Simple4DSTEMParams.model_validate(params)
+    as_json = params.model_dump_json()
+    print(as_json)
+    from_j = from_json(as_json)
+    pprint.pprint(type(from_j['overfocus']))
+    assert Simple4DSTEMParams.model_validate(from_j)
